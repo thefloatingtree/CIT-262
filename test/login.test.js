90b6e2989f2c18f4e2a1084e1a739310a6486548
thefloatingtree <thefloatingtree@gmail.com>
2022-05-10 10:21:07 -0600

    test failing test on gcr

--- test/login.test.js
+++ test/login.test.js
@@ -1,6 +1,6 @@
 import { login } from "../utils/login.js"
 
 it('Should get a login token', async () => {
-    const token = await login({ userName: "thefloatingtree@gmail.com", password: "696969Aa@7" })
+    const token = await login({ userName: "thefloatingtre@gmail.com", password: "696969Aa@7" })
     console.log(token)
 })
